e347eecf7b96b6484e577fba14082cdbb9cd46e7
baihuashu225 <yanpeng_a@aspirecn.com>
2019-06-19 14:31:07 +0800

    test git 1

---
+++ src/main/java/com/jelly/eoss/testgit/A.java
@@ -0,0 +1,7 @@
+package com.jelly.eoss.testgit;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.printf("test git 1");
+    }
+}
---
+++ src/main/java/com/jelly/eoss/testgit/B.java
@@ -0,0 +1,7 @@
+package com.jelly.eoss.testgit;
+
+public class B {
+    public static void main(String[] args) {
+        System.out.printf("test git 2");
+    }
+}
