777da98a07ace7682a39e91edd134c2fd43dbb19
yanpeng_a <yanpeng_a@aspirecn.com>
2019-06-19 14:54:36 +0800

    32432432

--- src/main/java/com/jelly/eoss/testgit/A.java
+++ src/main/java/com/jelly/eoss/testgit/A.java
@@ -2,10 +2,14 @@ package com.jelly.eoss.testgit;
 
 public class A {
     public static void main(String[] args) {
-        System.out.printf("test git 1");
+        System.out.printf("test g21321it 1");
     }
 
     public static void add(){
+        System.out.printf("test 321321git 1");
+    }
+
+    public static void add2(){
         System.out.printf("test git 1");
     }
 }
