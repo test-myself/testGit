633380e4dbd17836c5566bd0d221eebe108bc3ad
baihuashu116 <yanpeng_a@aspirecn.com>
2019-06-19 14:40:29 +0800

    test git 2

--- src/main/java/com/jelly/eoss/testgit/A.java
+++ src/main/java/com/jelly/eoss/testgit/A.java
@@ -4,4 +4,8 @@ public class A {
     public static void main(String[] args) {
         System.out.printf("test git 1");
     }
+
+    public static void add(){
+        System.out.printf("test git 1");
+    }
 }
